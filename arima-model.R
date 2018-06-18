86f0830290dc600a559423db30d6c9a0f85bd40c
Benoît Nouyrigat <benoit.nouyrigat@zenika.com>
2018-06-18 22:02:11 +0200

    Initialize ARIMA model

---
+++ arima-model.R
@@ -0,0 +1,40 @@
+library('ggplot2')
+library('forecast')
+library('tseries')
+
+daily_data = read.csv('./day.csv', header=TRUE, stringsAsFactors=FALSE)
+daily_data$Date = as.Date(daily_data$dteday)
+
+ggplot(daily_data, aes(Date, cnt)) + geom_line() + scale_x_date('month')  + ylab("Daily Bike Checkouts") + xlab("")
+
+count_ts = ts(daily_data[, c('cnt')])
+daily_data$clean_cnt = tsclean(count_ts)
+
+ggplot() + geom_line(data = daily_data, aes(x = Date, y = clean_cnt)) + ylab('Cleaned Bicycle Count')
+
+daily_data$cnt_ma = ma(daily_data$clean_cnt, order=7) # using the clean count with no outliers
+daily_data$cnt_ma30 = ma(daily_data$clean_cnt, order=30)
+
+ggplot() +
+  geom_line(data = daily_data, aes(x = Date, y = clean_cnt, colour = "Counts")) +
+  geom_line(data = daily_data, aes(x = Date, y = cnt_ma,   colour = "Weekly Moving Average"))  +
+  geom_line(data = daily_data, aes(x = Date, y = cnt_ma30, colour = "Monthly Moving Average"))  +
+  ylab('Bicycle Count')
+
+count_ma = ts(na.omit(daily_data$cnt_ma), frequency=30)
+decomp = stl(count_ma, s.window="periodic")
+deseasonal_cnt <- seasadj(decomp)
+
+plot(decomp)
+
+adf.test(count_ma, alternative = "stationary")
+
+Acf(count_ma, main='')
+Pacf(count_ma, main='')
+
+count_d1 = diff(deseasonal_cnt, differences = 1)
+plot(count_d1)
+adf.test(count_d1, alternative = "stationary")
+
+Acf(count_d1, main='ACF for Differenced Series')
+Pacf(count_d1, main='PACF for Differenced Series')
